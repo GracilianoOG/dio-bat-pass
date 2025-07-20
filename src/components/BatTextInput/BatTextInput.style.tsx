49fd802e738b4315536ce623777db9a25705038b
Gabriel <gabriel.gracilianomb@gmail.com>
2025-07-20 16:17:17 -0300

    Style text input

--- src/components/BatTextInput/BatTextInput.style.tsx
+++ src/components/BatTextInput/BatTextInput.style.tsx
@@ -2,6 +2,14 @@ import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
   input: {
-    backgroundColor: "red",
+    backgroundColor: "#E5BF3C",
+    borderWidth: 1,
+    borderRadius: 5,
+    color: "#000",
+    fontSize: 20,
+    minHeight: 40,
+    padding: 10,
+    textAlign: "center",
+    width: "100%",
   },
 });
