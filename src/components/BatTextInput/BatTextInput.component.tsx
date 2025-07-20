f1da0cf2820a6e8ec4138f81db8005bfbca6529d
Gabriel <gabriel.gracilianomb@gmail.com>
2025-07-20 16:11:09 -0300

    Create BatTextInput component

---
+++ src/components/BatTextInput/BatTextInput.component.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { TextInput, View } from "react-native";
+
+export const BatTextInput = () => {
+  return (
+    <View>
+      <TextInput placeholder="pass" />
+    </View>
+  );
+};
+
+export default BatTextInput;
