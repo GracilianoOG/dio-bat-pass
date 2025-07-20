14b3ca91a18cbd858ef1cf9fda50246670e1c711
Gabriel <gabriel.gracilianomb@gmail.com>
2025-07-20 16:48:54 -0300

    Create button component

---
+++ src/components/BatButton/BatButton.component.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Pressable, Text, View } from "react-native";
+
+import { styles } from "./BatButton.style";
+
+const BatButton = () => {
+  return (
+    <View>
+      <Pressable style={styles.button} onPress={() => console.log("hello")}>
+        <Text style={styles.text}>GENERATE</Text>
+      </Pressable>
+      <Pressable style={styles.button} onPress={() => console.log("hello")}>
+        <Text style={styles.text}>🦇 COPY</Text>
+      </Pressable>
+    </View>
+  );
+};
+
+export default BatButton;
