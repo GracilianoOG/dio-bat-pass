791cd8e2003c699f829239ca857fe271c12d4ff8
Gabriel <gabriel.gracilianomb@gmail.com>
2025-07-20 16:12:46 -0300

    Add BatTextInput to home screen

--- src/screens/Home/Home.component.tsx
+++ src/screens/Home/Home.component.tsx
@@ -2,11 +2,13 @@ import { StatusBar } from "expo-status-bar";
 import { Text, View } from "react-native";
 import styles from "./Home.style";
 import BatLogo from "../../components/BatLogo/BatLogo.component";
+import BatTextInput from "../../components/BatTextInput/BatTextInput.component";
 
 const Home = () => {
   return (
     <View style={styles.container}>
       <BatLogo />
+      <BatTextInput />
       <Text>Component works!</Text>
       <StatusBar style="auto" />
     </View>
