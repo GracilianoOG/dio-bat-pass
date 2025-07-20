750c42ad92c171c5659ec277c73d5927bd09533d
Gabriel <gabriel.gracilianomb@gmail.com>
2025-07-20 16:48:40 -0300

    Create button styles

---
+++ src/components/BatButton/BatButton.style.tsx
@@ -0,0 +1,21 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  button: {
+    alignItems: "center",
+    backgroundColor: "#000",
+    borderRadius: 4,
+    elevation: 3,
+    justifyContent: "center",
+    paddingHorizontal: 32,
+    paddingVertical: 12,
+    width: "100%",
+  },
+  text: {
+    color: "#E5BF3C",
+    fontSize: 16,
+    fontWeight: "bold",
+    lineHeight: 21,
+    letterSpacing: 0.25,
+  },
+});
