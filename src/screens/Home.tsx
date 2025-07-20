eedc6c2ac1b0754e9e43cdb1889ac935217cc21d
Gabriel <gabriel.gracilianomb@gmail.com>
2025-07-20 11:57:04 -0300

    Create Home screen

---
+++ src/screens/Home.tsx
@@ -0,0 +1,13 @@
+import { StatusBar } from "expo-status-bar";
+import { Text } from "react-native";
+
+const Home = () => {
+  return (
+    <>
+      <Text>Component works!</Text>
+      <StatusBar style="auto" />
+    </>
+  );
+};
+
+export default Home;
