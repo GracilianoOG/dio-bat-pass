9844c03f9618be9d45f991171c94ab6a5cb9527a
Gabriel <gabriel.gracilianomb@gmail.com>
2025-07-20 17:48:09 -0300

    Move generator logic to BatGenerator component

---
+++ src/components/BatGenerator/BatGenerator.component.tsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import { View } from "react-native";
+import { styles } from "./BatGenerator.style";
+import BatTextInput from "../BatTextInput/BatTextInput.component";
+import BatButton from "../BatButton/BatButton.component";
+import * as Clipboard from "expo-clipboard";
+import generatePassword from "../../services/passwordService";
+
+export function BatGenerator() {
+  const [pass, setPass] = useState("");
+
+  const handlePasswordGeneration = () => {
+    const generatedToken = generatePassword();
+    setPass(generatedToken);
+  };
+
+  const handleCopy = async () => {
+    await Clipboard.setStringAsync(pass);
+  };
+
+  return (
+    <View style={styles.container}>
+      <BatTextInput pass={pass} />
+      <BatButton onPress={handlePasswordGeneration}>GENERATE</BatButton>
+      <BatButton onPress={handleCopy}>⚡ COPY</BatButton>
+    </View>
+  );
+}
--- src/screens/Home/Home.component.tsx
+++ src/screens/Home/Home.component.tsx
@@ -1,33 +1,14 @@
 import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
+import { View } from "react-native";
 import styles from "./Home.style";
 import BatLogo from "../../components/BatLogo/BatLogo.component";
-import BatTextInput from "../../components/BatTextInput/BatTextInput.component";
-import BatButton from "../../components/BatButton/BatButton.component";
-import { useState } from "react";
-import generatePassword from "../../services/passwordService";
-import * as Clipboard from "expo-clipboard";
+import { BatGenerator } from "../../components/BatGenerator/BatGenerator.component";
 
 const Home = () => {
-  const [pass, setPass] = useState("");
-
-  const handlePasswordGeneration = () => {
-    const generatedToken = generatePassword();
-    setPass(generatedToken);
-  };
-
-  const handleCopy = async () => {
-    await Clipboard.setStringAsync(pass);
-  };
-
   return (
     <View style={styles.container}>
       <BatLogo />
-      <View style={styles.generatorContainer}>
-        <BatTextInput pass={pass} />
-        <BatButton onPress={handlePasswordGeneration}>GENERATE</BatButton>
-        <BatButton onPress={handleCopy}>⚡ COPY</BatButton>
-      </View>
+      <BatGenerator />
       <StatusBar style="light" />
     </View>
   );
