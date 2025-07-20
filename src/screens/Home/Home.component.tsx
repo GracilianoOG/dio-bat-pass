e6a0b39901b3757946fcb9377b82bfa5cee3a148
Gabriel <gabriel.gracilianomb@gmail.com>
2025-07-20 12:41:34 -0300

    Import BatLogo.component

--- src/screens/Home/Home.component.tsx
+++ src/screens/Home/Home.component.tsx
@@ -1,10 +1,12 @@
 import { StatusBar } from "expo-status-bar";
 import { Text, View } from "react-native";
 import styles from "./Home.style";
+import BatLogo from "../../components/BatLogo/BatLogo.component";
 
 const Home = () => {
   return (
     <View style={styles.container}>
+      <BatLogo />
       <Text>Component works!</Text>
       <StatusBar style="auto" />
     </View>
