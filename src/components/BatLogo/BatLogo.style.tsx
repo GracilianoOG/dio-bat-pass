9f0b2d9fbedfd8c9bc53b945edd0c51fc6b1e745
Gabriel <gabriel.gracilianomb@gmail.com>
2025-07-20 13:00:07 -0300

    Style logo container

--- src/components/BatLogo/BatLogo.style.tsx
+++ src/components/BatLogo/BatLogo.style.tsx
@@ -1,6 +1,17 @@
 import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
+  container: {
+    alignSelf: "center",
+    backgroundColor: "#4D4D4D",
+    borderColor: "#FFF",
+    borderWidth: 2,
+    flexDirection: "column",
+    justifyContent: "center",
+    marginBottom: 60,
+    paddingTop: 20,
+    paddingBottom: 10,
+  },
   title: {
     textAlign: "center",
     fontWeight: "bold",
