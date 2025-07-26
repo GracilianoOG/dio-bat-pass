f1e1f790dcf7502486ca85eb57dbaf734321a5de
Gabriel <gabriel.gracilianomb@gmail.com>
2025-07-26 19:06:13 -0300

    Replace view with SafeAreaView

--- src/screens/HomeScreen/HomeScreen.component.tsx
+++ src/screens/HomeScreen/HomeScreen.component.tsx
@@ -1,16 +1,16 @@
 import { StatusBar } from "expo-status-bar";
-import { View } from "react-native";
 import styles from "./HomeScreen.style";
 import BatLogo from "../../components/BatLogo/BatLogo.component";
 import { BatGenerator } from "../../components/BatGenerator/BatGenerator.component";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const HomeScreen = () => {
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <BatLogo />
       <BatGenerator />
       <StatusBar style="light" />
-    </View>
+    </SafeAreaView>
   );
 };
 
