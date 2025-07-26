83b635b143ba4f8f6cc065b298bac1ba9356d37e
Gabriel <gabriel.gracilianomb@gmail.com>
2025-07-26 16:43:59 -0300

    Create ConfigScreen

---
+++ src/screens/ConfigScreen/ConfigScreen.component.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+import { styles } from "./ConfigScreen.style";
+
+export function ConfigScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>HELLO CONFIG</Text>
+    </View>
+  );
+}
