6b62ab09544413d210ab7bab13e02d0d73157d2a
Gabriel <gabriel.gracilianomb@gmail.com>
2025-07-26 17:25:10 -0300

    Add title prop

--- src/components/SwitchConfig/SwitchConfig.component.tsx
+++ src/components/SwitchConfig/SwitchConfig.component.tsx
@@ -3,13 +3,17 @@ import { Switch, Text, View } from "react-native";
 
 import { styles } from "./SwitchConfig.style";
 
-export function SwitchConfig() {
+interface SwitchConfigProps {
+  title: string;
+}
+
+export function SwitchConfig({ title }: SwitchConfigProps) {
   const [isEnabled, setIsEnabled] = useState(false);
   const toggleSwitch = () => setIsEnabled(prevState => !prevState);
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Uppercase characters</Text>
+      <Text style={styles.title}>{title}</Text>
       <Switch
         thumbColor={isEnabled ? "#E5BF3C" : "#aba4b8"}
         onValueChange={toggleSwitch}
