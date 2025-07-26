f8926a5841a3426e65b0786f9a58152471707424
Gabriel <gabriel.gracilianomb@gmail.com>
2025-07-26 17:22:57 -0300

    Switch state on press
    Custom thumb color provided.

--- src/components/SwitchConfig/SwitchConfig.component.tsx
+++ src/components/SwitchConfig/SwitchConfig.component.tsx
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { Switch, Text, View } from "react-native";
 
 import { styles } from "./SwitchConfig.style";
 
 export function SwitchConfig() {
+  const [isEnabled, setIsEnabled] = useState(false);
+  const toggleSwitch = () => setIsEnabled(prevState => !prevState);
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Uppercase characters</Text>
-      <Switch />
+      <Switch
+        thumbColor={isEnabled ? "#E5BF3C" : "#aba4b8"}
+        onValueChange={toggleSwitch}
+        value={isEnabled}
+      />
     </View>
   );
 }
