1fa7a519a9fa22504f4f6c44f053578cf5788324
Gabriel <gabriel.gracilianomb@gmail.com>
2025-07-26 19:00:44 -0300

    Create password patterns constant

---
+++ src/constants/passwordPatterns.ts
@@ -0,0 +1,4 @@
+export const UPPERCASE_LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+export const LOWERCASE_LETTERS = "abcdefghijklmnopqrstuvwxyz";
+export const NUMERIC_CHARACTERS = "0123456789";
+export const SPECIAL_CHARACTERS = "!@#$%&*";
