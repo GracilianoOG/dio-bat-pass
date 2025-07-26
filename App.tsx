f422504511c55aaa6cd904259aeb9107dc6108fb
Gabriel <gabriel.gracilianomb@gmail.com>
2025-07-26 16:46:04 -0300

    Add bottom tabs with Home and Config options
    The tabs received a starting style.

--- App.tsx
+++ App.tsx
@@ -1,5 +1,47 @@
+import { NavigationContainer } from "@react-navigation/native";
 import Home from "./src/screens/Home/Home.component";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { ConfigScreen } from "./src/screens/ConfigScreen/ConfigScreen.component";
+
+const { Navigator, Screen } = createBottomTabNavigator();
 
 export default function App() {
-  return <Home />;
+  return (
+    <NavigationContainer>
+      <Navigator
+        screenOptions={{
+          headerShown: false,
+          tabBarActiveTintColor: "#E5BF3C",
+          tabBarStyle: {
+            backgroundColor: "black",
+            borderTopWidth: 1,
+          },
+          tabBarLabelStyle: {
+            fontSize: 14,
+            textTransform: "uppercase",
+          },
+        }}
+      >
+        <Screen
+          name="Home"
+          component={Home}
+          options={{
+            tabBarIcon: ({ color }) => (
+              <Ionicons name="home" size={24} color={color} />
+            ),
+          }}
+        ></Screen>
+        <Screen
+          name="Config"
+          component={ConfigScreen}
+          options={{
+            tabBarIcon: ({ color }) => (
+              <Ionicons name="cog" size={24} color={color} />
+            ),
+          }}
+        ></Screen>
+      </Navigator>
+    </NavigationContainer>
+  );
 }
