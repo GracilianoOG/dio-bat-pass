1fc17b34eeb29d817631e7f6edcc678bdb3a333e
Gabriel <gabriel.gracilianomb@gmail.com>
2025-07-26 17:10:27 -0300

    Create base for config switch

--- src/screens/ConfigScreen/ConfigScreen.component.tsx
+++ src/screens/ConfigScreen/ConfigScreen.component.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Switch, Text, View } from "react-native";
 import { styles } from "./ConfigScreen.style";
 import { SafeAreaView } from "react-native-safe-area-context";
 
@@ -7,6 +7,26 @@ export function ConfigScreen() {
   return (
     <SafeAreaView style={styles.container}>
       <Text style={styles.mainTitle}>Settings</Text>
+      <View
+        style={{
+          width: "100%",
+        }}
+      >
+        <View
+          style={{
+            borderBottomWidth: 0.5,
+            borderBottomColor: "#fff",
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "space-between",
+          }}
+        >
+          <Text style={{ color: "#fff", textTransform: "uppercase" }}>
+            Uppercase characters
+          </Text>
+          <Switch />
+        </View>
+      </View>
     </SafeAreaView>
   );
 }
