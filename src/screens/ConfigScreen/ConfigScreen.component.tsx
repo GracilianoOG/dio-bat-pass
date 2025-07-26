c9b2a3261199038edc471fd04282da7cdd693992
Gabriel <gabriel.gracilianomb@gmail.com>
2025-07-26 18:22:58 -0300

    Get options context and pass props to switches
    - Check if context is valid;
    - Pass individual option to enable or disable the switch;
    - Pass the setter with correct option to execute when switch changes.

--- src/screens/ConfigScreen/ConfigScreen.component.tsx
+++ src/screens/ConfigScreen/ConfigScreen.component.tsx
@@ -1,18 +1,60 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Switch, Text, View } from "react-native";
 import { styles } from "./ConfigScreen.style";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { SwitchConfig } from "../../components/SwitchConfig/SwitchConfig.component";
+import { OptionsContext } from "../../contexts/OptionsContext";
 
 export function ConfigScreen() {
+  const optionsContext = useContext(OptionsContext);
+
+  if (!optionsContext) {
+    throw new Error("OptionsContext is undefined!");
+  }
+
+  const { options, setOptions } = optionsContext;
+
   return (
     <SafeAreaView style={styles.container}>
       <Text style={styles.mainTitle}>Settings</Text>
       <View style={styles.options}>
-        <SwitchConfig title="Uppercase characters" />
-        <SwitchConfig title="Lowercase characters" />
-        <SwitchConfig title="Numeric characters" />
-        <SwitchConfig title="Special characters" />
+        <SwitchConfig
+          title="Uppercase characters"
+          isSwitched={options.uppercase}
+          toggleSwitch={() =>
+            setOptions(prev => ({ ...prev, uppercase: !options.uppercase }))
+          }
+        />
+        <SwitchConfig
+          title="Lowercase characters"
+          isSwitched={options.lowercase}
+          toggleSwitch={() =>
+            setOptions(prev => ({
+              ...prev,
+              lowercase: !options.lowercase,
+            }))
+          }
+        />
+        <SwitchConfig
+          title="Numeric characters"
+          isSwitched={options.numeric}
+          toggleSwitch={() =>
+            setOptions(prev => ({
+              ...prev,
+              numeric: !options.numeric,
+            }))
+          }
+        />
+        <SwitchConfig
+          title="Special characters"
+          isSwitched={options.special}
+          toggleSwitch={() =>
+            setOptions(prev => ({
+              ...prev,
+              special: !options.special,
+            }))
+          }
+        />
       </View>
     </SafeAreaView>
   );
