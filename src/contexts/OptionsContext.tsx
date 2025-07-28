fa916d647ab42d6e485c60285532216d7f300a06
Gabriel <gabriel.gracilianomb@gmail.com>
2025-07-28 13:24:01 -0300

    Save and load settings using AsyncStorage

--- src/contexts/OptionsContext.tsx
+++ src/contexts/OptionsContext.tsx
@@ -1,5 +1,6 @@
-import { createContext, ReactNode, useState } from "react";
+import { createContext, ReactNode, useEffect, useState } from "react";
 import { OptionTypes } from "../types/optionTypes";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 interface OptionsContextProps {
   setOptions: React.Dispatch<React.SetStateAction<OptionTypes>>;
@@ -21,6 +22,44 @@ export const OptionsProvider = ({ children }: OptionsProviderProps) => {
     numeric: true,
     special: false,
   });
+
+  const loadOptions = async () => {
+    try {
+      const jsonOptions = await AsyncStorage.getItem("bat-settings");
+      const options: OptionTypes =
+        jsonOptions !== null ? JSON.parse(jsonOptions) : null;
+      return options;
+    } catch (e) {
+      console.log("Error while loading settings: ", e);
+    }
+  };
+
+  const saveOptions = async (options: OptionTypes) => {
+    try {
+      const jsonOptions = JSON.stringify(options);
+      await AsyncStorage.setItem("bat-settings", jsonOptions);
+    } catch (e) {
+      console.log("Error while saving settings: ", e);
+    }
+  };
+
+  useEffect(() => {
+    (async () => {
+      const loadedOptions = await loadOptions();
+
+      if (loadedOptions) {
+        setOptions(loadedOptions);
+        console.log("Loaded options from AsyncStorage");
+      }
+    })();
+  }, []);
+
+  useEffect(() => {
+    (async () => {
+      await saveOptions(options);
+      console.log("Saved options to AsyncStorage");
+    })();
+  }, [options]);
 
   return (
     <OptionsContext value={{ options, setOptions }}>{children}</OptionsContext>
